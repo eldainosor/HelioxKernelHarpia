cff0d598049deb86ceb49d3ca01dd133f3a4b28f
Yi-wei Zhao <gbjc64@motorola.com>
2012-12-14 16:34:53 -0600

    staging: android: lowmemorykiller: select a new task to kill
    Under certain circumstances, a process may take time to handle a SIGKILL.
    When lowmemkiller is called again shortly after, it would pick the same
    process to kill over and over, so that we cann't get free memory for long time.
    
    Solution is to check fatal_signal_pending() on the selected task, and if it's
    already pending, select a new task to kill.
    
    Signed-off-by: Tianshui Shi <kfp634@motorola.com>
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -540,6 +540,19 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			task_unlock(p);
 			continue;
 		}
+		if (fatal_signal_pending(p) ||
+				((p->flags & PF_EXITING) &&
+					test_tsk_thread_flag(p, TIF_MEMDIE))) {
+			lowmem_print(2, "skip slow dying process %d\n", p->pid);
+			task_unlock(p);
+			continue;
+		}
+		if (fatal_signal_pending(p)) {
+			lowmem_print(2, "skip slow dying process %d\n", p->pid);
+			task_unlock(p);
+			continue;
+		}
+>>>>>>> 7599f70... staging: android: lowmemorykiller: select a new task to kill
 		tasksize = get_mm_rss(p->mm);
 		task_unlock(p);
 		if (tasksize <= 0)
