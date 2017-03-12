9faefcc0f13dc3adfad8293ad168037f1a3a74be
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-03-12 13:49:38 +0530

    Release 1

--- heliox.sh
+++ heliox.sh
@@ -3,6 +3,28 @@ export SUBARCH=arm
 export CROSS_COMPILE="gcc-linaro/bin/arm-linux-gnueabihf-"
 export KBUILD_BUILD_USER="Subhrajyoti"
 export KBUILD_BUILD_HOST="Beast"
+HELIOX_VERSION="Release-1"
 make mrproper
 make heliox_defconfig
-./build_cwm_zip.sh
+
+set -e
+rm -f arch/arm/boot/dts/*.dtb
+rm -f arch/arm/boot/dt.img
+rm -f cwm_flash_zip/boot.img
+make -j4 zImage
+make -j4 dtimage
+make -j4 modules
+rm -rf heliox_install
+mkdir -p heliox_install
+make -j4 modules_install INSTALL_MOD_PATH=heliox_install INSTALL_MOD_STRIP=1
+mkdir -p cwm_flash_zip/system/lib/modules/pronto
+find heliox_install/ -name '*.ko' -type f -exec cp '{}' cwm_flash_zip/system/lib/modules/ \;
+mv cwm_flash_zip/system/lib/modules/wlan.ko cwm_flash_zip/system/lib/modules/pronto/pronto_wlan.ko
+cp arch/arm/boot/zImage cwm_flash_zip/tools/
+cp arch/arm/boot/dt.img cwm_flash_zip/tools/
+rm -f arch/arm/boot/heliox_kernel.zip
+cd cwm_flash_zip
+zip -r ../Heliox-harpia-$HELIOX_VERSION-$(date +"%Y%m%d")-$(date +"%H%M%S").zip ./
+
+
+
